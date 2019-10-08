bb7b16ea25e11e6441393ace3f80c16c19d318f0
Joshua A. Anderson <joaander@umich.edu>
2019-10-08 14:15:24 -0400

    Use patch to modify CMakeLists.txt

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Modify CMakeLists.txt so that no CXX compiler is required to install
-sed -i -e 's/project (cereal)/project (cereal NONE)/' CMakeLists.txt
-
 mkdir build
 cd build
 cmake ../ -DJUST_INSTALL_CEREAL=on -DCMAKE_INSTALL_PREFIX=${PREFIX}
