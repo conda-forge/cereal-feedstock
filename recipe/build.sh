3e959e144c2f1d86cb01393352c19021087234ef
Joshua A. Anderson <joaander@umich.edu>
2019-10-08 12:32:53 -0400

    Install cereal with cmake

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-cp -R include/cereal "$PREFIX/include"
+
+# Modify CMakeLists.txt so that no CXX compiler is required to install
+sed -i -e 's/project (cereal)/project (cereal NONE)/' CMakeLists.txt
+
+mkdir build
+cd build
+cmake ../ -DJUST_INSTALL_CEREAL=on -DCMAKE_INSTALL_PREFIX=${PREFIX}
+make install
